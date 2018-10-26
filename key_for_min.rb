7246d132ff687c6c32292181341159358412ef3a
Kaela Clement <stephanie.kaela@gmail.com>
2018-10-26 21:47:26 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,22 +6,12 @@ def key_for_min_value(name_hash)
     return nil
   end
 
-  # nums = name_hash.collect {|key, value| value}
-  # nums.sort!
-  # min = nums[0]
-  # name_hash.each do |key, value|
-    # if value == min
-    #  return key
-    # end
-  # end
-  lowest = nil
-  lowest_key = ""
-  sorted = name_hash.collect do |key,value|
-    lowest ||= value
-    if value < lowest
-      lowest = value
-      lowest_key = key
-    end
-  end
-  lowest_key
+   nums = name_hash.collect {|key, value| value}
+   nums.sort!
+   min = nums[0]
+   name_hash.each do |key, value|
+     if value == min
+      return key
+     end
+   end
 end
